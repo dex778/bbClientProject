175da5dea2c81b5dcb820e8d5e2aa3a18ffdad42
Kenneth Martinez <kenneth@Kenneths-MacBook-Pro.local>
2021-05-27 18:00:40 -0400

    added components and routes to files

---
+++ recipefrontend/src/components/LandingPage/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import CreateRecipe from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+
+
+const Login = () => {
+
+
+    return ( 
+       <div>
+            {/* <div>
+            
+            <Link to='/createrecipe'>Create Recipe</Link>
+            </div>
+            <div>
+                <Switch>
+                    <Route exact path='/createRecipe'><CreateRecipe /></Route>
+                </Switch>
+            </div> */}
+            
+        </div>
+     );
+}
+ 
+export default Login;
