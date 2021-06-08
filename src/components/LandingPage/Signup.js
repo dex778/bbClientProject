a2b9991a9b93633c012b21673ec58991a9f087ff
Dex778 <43555123+dex778@users.noreply.github.com>
2021-06-08 13:32:32 -0400

    changed character required length, added username length condition

--- src/components/LandingPage/Signup.js
+++ src/components/LandingPage/Signup.js
@@ -6,8 +6,10 @@ const Signup = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [passlength, setPasslength] = useState(null);
+    const [userlength, setUserlength] = useState(null);
 
-    requiredLength = 8
+
+    requiredLength = 5
 
     let handleSubmit = (event) => {
         event.preventDefault();
@@ -32,7 +34,8 @@ const Signup = (props) => {
     }
 
     useEffect(() => {
-        setPasslength(password.length < requiredLength ? true : alert('Your password is too short'))
+        setPasslength(password.length < requiredLength ? true : alert('Your password is too short'));
+        setUserlength(userlength.length < requiredLength ? true : alert('Your username is too short'));
         // setValidLength(firstPassword.length >= requiredLength ? true : false)
 
     },[])
